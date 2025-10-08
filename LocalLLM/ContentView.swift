55c187328612fdfcfe408411f1c5966c9c7df62b
shake551 <hiroki.yamako@gmail.com>
2025-10-08 21:57:15 +0900

    Initial Commit

---
+++ LocalLLM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocalLLM
+//
+//  Created by shake on 2025/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocalLLM/LocalLLMApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalLLMApp.swift
+//  LocalLLM
+//
+//  Created by shake on 2025/10/08.
+//
+
+import SwiftUI
+
+@main
+struct LocalLLMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
